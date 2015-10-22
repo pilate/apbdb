bf0cff4d6c464a5d489b6ed352fc3b7e4dbc2aca
Pilate <pilate@pilate.es>
2015-10-21 20:52:10 -0700

    inherit from base

--- apbdb/settings.py
+++ apbdb/settings.py
@@ -37,7 +37,7 @@ ROOT_URLCONF = 'apbdb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['apbdb/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
